42891a5fa2b3d4cefee07c348393c8b5ce485372
Olga Savkova <olga.savkova@2me.link>
2018-10-28 14:55:19 +0200

    added doGet() for survey/form

--- src/main/java/com/savkova/app/SurveyServlet.java
+++ src/main/java/com/savkova/app/SurveyServlet.java
@@ -41,6 +41,12 @@ public class SurveyServlet extends HttpServlet
         request.getRequestDispatcher("report.jsp").forward(request, response);
     }
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        request.getRequestDispatcher("survey.jsp").forward(request, response);
+    }
+
     private void countRating(Person person, Answer[] answers)
     {
         int currentYear = LocalDate.now().getYear();
@@ -64,55 +70,4 @@ public class SurveyServlet extends HttpServlet
         }
     }
 
-    private enum AgeGroup
-    {
-        AGE_GROUP25(0, 25),
-        AGE_GROUP35(25, 35),
-        AGE_GROUP45(35, 45),
-        AGE_GROUP55(45, 65),
-        AGE_GROUP65(65, 150);
-
-        final private int minAge;
-        final private int maxAge;
-        private int votes;
-        private double rating;
-
-        AgeGroup(final int minAge, final int maxAge)
-        {
-            this.minAge = minAge;
-            this.maxAge = maxAge;
-        }
-
-        public int getMinAge()
-        {
-            return minAge;
-        }
-
-        public int getMaxAge()
-        {
-            return maxAge;
-        }
-
-        public int getVotes()
-        {
-            return votes;
-        }
-
-        public double getRating()
-        {
-            return rating;
-        }
-
-        private void updateRating(final double rate)
-        {
-            this.rating += rate;
-            this.votes += 1;
-        }
-
-        @Override
-        public String toString()
-        {
-            return String.format("%3d - %3d", minAge, maxAge);
-        }
-    }
 }
